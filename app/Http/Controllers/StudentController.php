1fac50a72c52eeb932395bce6604b1e2c5879365
sirzes02 <sirzes02@gmail.com>
2020-08-14 19:36:11 +0000

    first

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,15 +5,11 @@ namespace App\Http\Controllers;
 use App\Question;
 use App\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
+     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -42,7 +38,19 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+	    $student = new Student();
+
+	    $student->puntaje = 0;
+	    $student->resultas = "";
+	    $student->semana = "";
+	    $student->cedula = $request->get("cedula");
+	    $student->email = $request->get("email");
+	    $student->avatar = $reques->get("avatar");
+	    $student->nombre = $request->get("nombre");
+	    $student->contrasenia = bcrypt($request->get("contrasenia"));
+	    $student->tipo = $request->get("tipo");
+	    $student->programa = $request->get("programa");
+	    $student->facultad = $request->get("facultad");
     }
 
     /**
@@ -88,5 +96,96 @@ class StudentController extends Controller
     public function destroy(Request $request)
     {
         //
+    }
+
+    public function login(Request $request)
+    {
+	    $student = Student::where('email', '=', $request->get("email"))->first();
+
+	    if (!$student)
+	    {
+		return response()->json(["ingreso"=>false]);
+	    }
+
+	   if(Hash::check($request->get("contrasenia"), $student->contrasenia)){ 
+	   	return response()->json(["cedula"=>$student->cedula, "nombre"=>$student->nombre, "avatar"=>$student->avatar, "semanas"=>$student->semanas]);
+	   }
+
+	   return response()->json(["ingreso"=>false]);
+    }
+
+    public function register(Request $request)
+    {
+	$student = new Student();
+
+	$student->puntaje = 0;
+	$student->resueltas = "";
+	$student->semana = "";
+	$student->cedula = $request->get("cedula");
+	$student->email = $request->get("email");
+	$student->avatar = $request->get("avatar");
+	$student->nombre = $request->get("nombre");
+	$student->contrasenia = bcrypt($request->get("contrasenia"));
+	$student->tipo = $request->get("tipo");
+	$student->programa = $request->get("programa");
+	$student->facultad = $request->get("facultad");
+
+	$student->save();
+
+	return response()->json(["ingreso"=>"Registro exitoso"]);
+    }
+
+    public function resueltas(Request $request)
+    {
+	$student = Student::find($request->get("id"));
+
+	if (!$student)
+	{
+		return response()->json(["error"=>"Estudiante no encontrado"]);
+	}
+
+	return response()->json(["resueltas"=>$student->resueltas]);
+    }
+
+    public function resueltasUpdate(Request $request){
+	$student = Student::find($request->get("id"));
+
+	if (!$student)
+	{
+		return response()->json(["error"=>"Estudiante no encontrado"]);
+	}
+
+	$student->resueltas = $request->get("resueltas");
+
+	$student->update();
+
+	return response()->json(["Success"=>$student->email. " fue actualizado"]);
+    }
+
+    public function semanas(Request $request)
+    {
+	$student = Student::find($request->get("id"));
+
+	if (!$student)
+	{
+		return response()->json(["error"=>"Estudiante no encontrado"]);
+	}
+
+	return response()->json(["semana"=>$student->semana]);	
+    }
+
+    public function semanasUpdate(Request $request){
+	$student = Student::find($request->get("id"));
+
+	if (!$student)
+	{
+		return response()->json(["error"=>"Estudiante no encontrado"]);
+	}
+
+	$student->semana = $request->get("semana");
+
+	$student->update();
+
+	return response()->json(["Success"=>$student->email. " fue actualizado"]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,15 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::resource('/users', 'UserController');
 Route::resource('/roles', 'RoleController');
 Route::resource('/questions', 'QuestionController');
-Route::resource('/students', 'StudentController');
+Route::post('/students/login', 'StudentController@login');
+Route::post('/students/register', 'StudentController@register');
+Route::post('/students/resueltas', 'StudentController@resueltas');
+Route::post('/students/resueltasUpdate', 'StudentController@resueltasUpdate');
+Route::post('/students/semanas', 'StudentController@semanas');
+Route::post('/students/semanasUpdate', 'StudentController@semanasUpdate');
+Route::resource('/students', 'StudentController')->middleware(("auth"));
 
 Route::get('/information', function () {
     return view("information.index");
 })->middleware(("auth"));
+
